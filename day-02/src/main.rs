c9815ed6b151aadac026868392a224526b259139
naiker <yinho999@gmail.com>
2023-01-22 21:54:57 +0000

    day 2

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -33,44 +33,77 @@ impl RPS {
 }
 
 trait Match {
-    fn play(&self, other: &RPS) -> usize;
+    fn user_fight_opponent(&self, other: &RPS) -> usize;
+    fn predict_user_hand_by_result(&self, match_result: EndOfRound) -> usize;
 }
 
 impl Match for RPS {
-    fn play(&self, other: &RPS) -> usize {
+    fn user_fight_opponent(&self, other: &RPS) -> usize {
         let result = match self {
             RPS::Rock => match other {
-                RPS::Rock => Result::Draw,
-                RPS::Paper => Result::Lose,
-                RPS::Scissors => Result::Win,
+                RPS::Rock => EndOfRound::Draw,
+                RPS::Paper => EndOfRound::Lose,
+                RPS::Scissors => EndOfRound::Win,
             },
             RPS::Paper => match other {
-                RPS::Rock => Result::Win,
-                RPS::Paper => Result::Draw,
-                RPS::Scissors => Result::Lose,
+                RPS::Rock => EndOfRound::Win,
+                RPS::Paper => EndOfRound::Draw,
+                RPS::Scissors => EndOfRound::Lose,
             },
             RPS::Scissors => match other {
-                RPS::Rock => Result::Lose,
-                RPS::Paper => Result::Win,
-                RPS::Scissors => Result::Draw,
+                RPS::Rock => EndOfRound::Lose,
+                RPS::Paper => EndOfRound::Win,
+                RPS::Scissors => EndOfRound::Draw,
             },
         };
-        Result::from_result(result) + self.get_value()
+        EndOfRound::from_result_to_score(result) + self.get_value()
+    }
+
+    // self is opponent, match result is the result for player
+    fn predict_user_hand_by_result(&self, match_result: EndOfRound) -> usize {
+        let user_hand = match match_result {
+            EndOfRound::Win => match self {
+                RPS::Rock => RPS::Paper,
+                RPS::Paper => RPS::Scissors,
+                RPS::Scissors => RPS::Rock,
+            },
+            EndOfRound::Lose => match self {
+                RPS::Rock => RPS::Scissors,
+                RPS::Paper => RPS::Rock,
+                RPS::Scissors => RPS::Paper,
+            },
+            EndOfRound::Draw => match self {
+                RPS::Rock => RPS::Rock,
+                RPS::Paper => RPS::Paper,
+                RPS::Scissors => RPS::Scissors,
+            },
+        };
+        user_hand.get_value() + EndOfRound::from_result_to_score(match_result)
     }
 }
 
-enum Result {
+#[derive(Clone, Copy)]
+enum EndOfRound {
     Win,
     Lose,
     Draw,
 }
 
-impl Result {
-    fn from_result(result: Result) -> usize {
+impl EndOfRound {
+    fn from_result_to_score(result: EndOfRound) -> usize {
         match result {
-            Result::Win => 6,
-            Result::Lose => 0,
-            Result::Draw => 3,
+            EndOfRound::Win => 6,
+            EndOfRound::Lose => 0,
+            EndOfRound::Draw => 3,
+        }
+    }
+
+    fn from_string_to_EOR(input: &str) -> Self {
+        match input {
+            "X" => EndOfRound::Lose,
+            "Y" => EndOfRound::Draw,
+            "Z" => EndOfRound::Win,
+            _ => panic!("Invalid input, unable to convert to end of round"),
         }
     }
 }
@@ -89,12 +122,27 @@ fn part_one(file_name: &str) -> usize {
         let mut line = line.split(" ");
         let opponent = RPS::from_opponent_string(line.next().unwrap());
         let player = RPS::from_player_string(line.next().unwrap());
-        score += player.play(&opponent);
+        score += player.user_fight_opponent(&opponent);
     }
     score
 }
 
-fn main() {}
+fn part_two(file_name: &str) -> usize {
+    let file = load_file(file_name);
+    let mut score = 0;
+    for line in file {
+        let mut line = line.split(" ");
+        let opponent = RPS::from_opponent_string(line.next().unwrap());
+        let match_result = EndOfRound::from_string_to_EOR(line.next().unwrap());
+        score += opponent.predict_user_hand_by_result(match_result);
+    }
+    score
+}
+
+fn main() {
+    let score = part_two("data.txt");
+    println!("{}", score);
+}
 
 // Testing
 #[cfg(test)]
@@ -102,7 +150,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_total_score() {
+    fn test_part_one() {
         let score = part_one("example.txt");
         assert_eq!(score, 15);
     }
